2901473b24c8cfc0bc20a62cae0aeab40b9dc557
Damodred <marcin@damodred.net>
2011-06-27 23:32:19 -0700

    Added Express. Now got very basic nodechatty. More tomorrow.

--- server.js
+++ server.js
@@ -1,7 +1,42 @@
-var formidable = require('formidable'),
-    http = require('http'),
+var express = require('express')
+  , app = express.createServer();
 
-    sys = require('sys');
+app.use(express.bodyParser());
+
+var buffer = [];
+
+app.post('/', function(req, res, next) {
+  console.log(req.body.chat);
+  buffer.push(req.body.chat.content);
+  next();
+});
+
+app.all('/', function(req, res) {
+  var output;
+  for (var i in buffer) {
+    output += buffer[i] + '<br>';
+  }
+  res.send(
+    '<form action="/" method="post">'+
+    '<input type="text" name="chat[content]"><br>'+
+    '<input type="text" name="chat[name]"><br>'+
+    '<input type="submit" value="Upload">'+
+    '</form>' + output
+  );  
+}); 
+/*
+app.get('/', function(req, res) {
+  res.send(
+    '<form action="/post" enctype="multipart/form-data" method="post">'+
+    '<input type="text" name="textcontent"><br>'+
+    '<input type="submit" value="Upload">'+
+    '</form>'
+  );
+  //res.send('Hello World');
+});*/
+
+app.listen(3030);
+/*
 
 http.createServer(function(req, res) {
   if (req.method.toLowerCase() == 'post') {
@@ -22,11 +57,11 @@ http.createServer(function(req, res) {
   }
 
   //res.writeHead(200, {'content-type': 'text/html'});
-/*  res.write(
+  res.write(
     '<form action="" enctype="multipart/form-data" method="post">'+
     '<input type="text" name="title"><br>'+
     '<input type="submit" value="Upload">'+
     '</form>'
   );
-  res.end();*/
-}).listen(1337);
+  res.end();
+}).listen(1337);*/
